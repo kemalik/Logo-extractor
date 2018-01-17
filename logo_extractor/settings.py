f48a090e27b0eaf7ec9ddeb2b05f3dcde6ee9a6e
Malik <kekenov.malik@gmail.com>
2018-01-17 22:50:23 +0600

    added env vars for database

--- logo_extractor/settings.py
+++ logo_extractor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(2+t6pxem!yz8t9y8qzdym^k!ne!lqmt-9yq$14d%rjalot(0-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'logo_extractor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', 3306),
     }
 }
 
