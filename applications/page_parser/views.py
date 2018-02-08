d57f7e0e912a8846e12f2ec5fe6c92917fc186a5
Malik <kekenov.malik@gmail.com>
2018-02-08 21:43:58 +0600

    added result view and template

--- applications/page_parser/views.py
+++ applications/page_parser/views.py
@@ -1,1 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class ResultView(TemplateView):
+    template_name = "result.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['result'] = 'Hello world'
+        return context
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from applications.page_parser.views import ResultView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ResultView.as_view()),
 ]
